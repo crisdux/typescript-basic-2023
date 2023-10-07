f74a4db146d047509d283880b4b34433863c1bc1
crisdux <crisfer.4217@gmail.com>
2023-10-06 23:33:35 -0400

    tuplas

---
+++ 02-tipos/tupla.js
@@ -0,0 +1,7 @@
+"use strict";
+const temp = [20, "F"];
+temp.forEach((item, index) => {
+    console.log(item);
+});
+temp.push(30);
+console.log(temp);
---
+++ 02-tipos/tupla.ts
@@ -0,0 +1,7 @@
+const temp:[number, string] = [20,"F"];
+temp.forEach((item, index) => {
+    console.log(item);
+});
+
+temp.push(30);
+console.log(temp);
