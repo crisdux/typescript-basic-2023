d0ea2efa9282ab8ab91c6a7b15c76250c350434e
crisdux <crisfer.4217@gmail.com>
2023-10-10 01:33:41 -0400

    polimorfismo y sobre carga de metodos

---
+++ 06-clases/07-poli.js
@@ -0,0 +1,36 @@
+"use strict";
+(() => {
+    class Animal {
+        constructor(nombre, edad) {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+        hacerSonido() {
+            return "Sonido generico";
+        }
+    }
+    class Perro extends Animal {
+        constructor(nombre, edad, isCola = true) {
+            super(nombre, edad);
+            this.isCola = true;
+            this.isCola = isCola;
+        }
+        hacerSonido() {
+            return "Guau Guau";
+        }
+    }
+    class Gato extends Animal {
+        constructor(nombre, edad, color) {
+            super(nombre, edad);
+            this.color = color;
+        }
+        hacerSonido() {
+            return "miau miau";
+        }
+    }
+    const miPerro = new Perro("boby", 5);
+    console.log(miPerro);
+    console.log(miPerro.hacerSonido());
+    const miGato = new Gato("michi", 3, "cafe");
+    console.log(miGato.hacerSonido());
+})();
---
+++ 06-clases/07-poli.ts
@@ -0,0 +1,52 @@
+
+
+
+(() => {
+    class Animal {
+        protected nombre:string;
+        protected edad:number;
+
+        constructor(nombre:string, edad:number){
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+
+        hacerSonido(): string{
+            return "Sonido generico";
+        }
+    }
+
+    class Perro extends Animal {
+        public isCola:boolean = true;
+        constructor(nombre:string, edad:number, isCola:boolean = true){
+            super(nombre, edad);
+            this.isCola = isCola;
+        }
+
+        hacerSonido(): string {
+            return "Guau Guau";
+        }
+    }
+
+    class Gato extends Animal {
+        private color:string;
+        constructor(nombre:string, edad:number, color:string){
+            super(nombre, edad);
+            this.color = color;
+        }
+
+        hacerSonido(): string {
+            return "miau miau";
+        }
+    }
+
+
+    
+
+    const miPerro = new Perro("boby", 5);
+    console.log(miPerro);
+    console.log(miPerro.hacerSonido())
+
+    const miGato = new Gato("michi", 3, "cafe");
+    console.log(miGato.hacerSonido());
+})();
